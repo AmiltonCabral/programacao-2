52769736b26ce80e1f8f2d9535dd94893b614939
AmiltonCristian <amilton.cabral@ccc.ufcg.edu.br>
2022-01-05 14:46:30 -0300

    added lab03

---
+++ lab03/src/agenda/Agenda.java
@@ -0,0 +1,50 @@
+package agenda;
+
+/**
+ * Uma agenda que mantém uma lista de contatos com posições. Podem existir 100 contatos. 
+ * 
+ * @author nazareno
+ *
+ */
+public class Agenda {
+	
+	private static final int TAMANHO_AGENDA = 100;
+	
+	private String[] contatos;
+
+	/**
+	 * Cria uma agenda.
+	 */
+	public Agenda() {
+		this.contatos = new String[TAMANHO_AGENDA];
+	}
+	
+	/**
+	 * Acessa a lista de contatos mantida.
+	 * @return O array de contatos.
+	 */
+	public String[] getContatos() {
+		return this.contatos.clone();
+	}
+
+	/**
+	 * Acessa os dados de um contato específico.
+	 * @param posicao Posição do contato na agenda.
+	 * @return Dados do contato. Null se não há contato na posição.
+	 */
+	public String getContato(int posicao) {
+		return contatos[posicao];
+	}
+
+	/**
+	 * Cadastra um contato em uma posição. Um cadastro em uma posição que já existe sobrescreve o anterior. 
+	 * @param posicao Posição do contato.
+	 * @param nome Nome do contato.
+	 * @param sobrenome Sobrenome do contato.
+	 * @param telefone Telefone do contato.
+	 */
+	public void cadastraContato(int posicao, String nome, String sobrenome, String telefone) {
+		this.contatos[posicao] = nome;
+	}
+
+}
